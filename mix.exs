2102b071de2d45a7b528fc4fc7172e62dc72ce50
toran billups <toranb@gmail.com>
2021-01-15 10:05:55 -0600

    upgrade to gleam 0.13.2

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Game.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
-      {:gleam_stdlib, "~> 0.12"},
+      {:gleam_stdlib, "~> 0.13"},
       {:mix_gleam, "~> 0.1.0"}
     ]
   end
